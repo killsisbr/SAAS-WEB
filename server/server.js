1d59c09ae07ac59da8e6c9f9b70383b97eef0e07
Lucas Larocca <killsisbr@gmail.com>
2026-01-07 15:44:44 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -32,8 +32,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware basico
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// Middleware basico
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // Seguranca
 app.use(helmet({
